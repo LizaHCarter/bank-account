e14b3c0ebfc3df9403f80a2f15524e6388df5f8d
Liza Carter <lizahcarter@gmail.com>
2014-07-10 06:14:27 -0500

    updated Bank Account program with chalk changes

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+/*Simple bank-account program*/
 var prompt = require('sync-prompt').prompt;
+var chalk = require('chalk');
 
+/*Set up starting balance and arrays*/
 var start = 1000;
 var withdraw = [];
 var deposit = [];
 
+/*Prompt user for first action*/
 var response = prompt('Your current balance is $1,000. Would you like to make a (d)eposit, (w)ithdrawal, or (q)uit and view your balance? ');
 
+/*Loops for when user wants to make deposit or withdraw*/
 while(response === 'd' || response === 'w'){
   if(response === 'd'){
     var amountD = prompt('Deposit amount? $');
@@ -20,29 +25,31 @@ while(response === 'd' || response === 'w'){
     response = prompt('What would you like to do next? (w,d,q) ');
   }
 }
-
+/*Generating new balance when user wants to quit*/
 if(response === 'q'){
   var sumD = 0;
   for(var D = 0; D < deposit.length; D ++){
     sumD += deposit[D];
   }
-  console.log('Deposit amounts: $'+deposit+ '.');
-  console.log('Total deposits: $'+sumD+ '.');
+  /*Optional:console.log('Deposit amounts: $'+deposit+ '.');*/
+  console.log(chalk.green('Total deposits: $'+sumD+ '.'));
 
   var sumW = 0;
   for(var W = 0; W < withdraw.length; W ++){
     sumW += withdraw[W];
   }
-    console.log('Withdrawal amounts: $'+withdraw+ '.');
-    console.log('Total withdrawals: - $'+sumW+ '.');
+    /* Optional:console.log('Withdrawal amounts: $'+withdraw+ '.');*/
+    console.log(chalk.red('Total withdrawals: - $'+sumW+ '.'));
 
   var balance = start + sumD - sumW;
 
+/*Applying the $50 overdraft fee when balance falls below $0*/
   if(balance < 0){
     balance += -50;
   }
 
-  console.log('Your new balance is $'+balance+'.');
+/*Letting user know new balance*/
+  console.log(chalk.yellow('Your new balance is $'+balance+'.'));
   }
 
 
